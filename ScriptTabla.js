84c6dad5b88dbb75e8ed574cfc9ae74c3a9c3b88
RobertCR <rob3rtcr@gmail.com>
2022-01-31 23:36:07 -0300

    Cambios en script varios

---
+++ ScriptTabla.js
@@ -0,0 +1,101 @@
+
+        var rIndex,
+        table = document.getElementById("table");
+        //---- chequeo que los inputs no esten vacios
+        function checkEmptyInput()
+        {
+            var isEmpty = false,
+                nombreProduct = document.getElementById("nombreProduct").value,
+                detalle = document.getElementById("detalle").value,
+                cantidad = document.getElementById("cantidad").value;
+                precio = document.getElementById("precio").value;
+                precio = document.getElementById("resultado").value;
+        
+            if(nombreProduct === ""){
+                alert("El nombre no puede estar vacio");
+                isEmpty = true;
+            }
+            else if(detalle === ""){
+                alert("El detalle no puede estar vacio");
+                isEmpty = true;
+            }
+            else if(cantidad === ""){
+                alert("Cantidad no puede estar vacio");
+                isEmpty = true;
+            }
+            else if(precio === ""){
+                alert("El precio no puede estar vacio");
+                isEmpty = true;
+            }
+            return isEmpty;
+        }
+            
+        // agregar producto
+        function addHtmlTableRow()
+        {
+            if(!checkEmptyInput()){
+            var newRow = table.insertRow(table.length),
+                cell1 = newRow.insertCell(0),
+                cell2 = newRow.insertCell(1),
+                cell3 = newRow.insertCell(2),
+                cell4 = newRow.insertCell(3)
+                cell5 = newRow.insertCell(4)
+                nombreProduct = document.getElementById("nombreProduct").value,
+                detalle = document.getElementById("detalle").value,
+                cantidad = document.getElementById("cantidad").value;
+                precio = document.getElementById("precio").value;
+                resultado = document.getElementById("resultado").value;
+            
+            cell1.innerHTML = nombreProduct;
+            cell2.innerHTML = detalle;
+            cell3.innerHTML = cantidad;
+            cell4.innerHTML = precio;
+            cell5.innerHTML = resultado;
+            selectedRowToInput();
+            }
+        }
+        
+        function selectedRowToInput()
+        {
+            
+            for(var i = 1; i < table.rows.length; i++)
+            {
+                table.rows[i].onclick = function()
+                {
+                    rIndex = this.rowIndex;
+                    document.getElementById("nombreProduct").value = this.cells[0].innerHTML;
+                    document.getElementById("detalle").value = this.cells[1].innerHTML;
+                    document.getElementById("cantidad").value = this.cells[2].innerHTML;
+                    document.getElementById("precio").value = this.cells[3].innerHTML;
+                    document.getElementById("resultado").value = this.cells[4].innerHTML;
+                };
+            }
+        }
+        selectedRowToInput();
+        
+        function editHtmlTbleSelectedRow()
+        {
+            var nombreProduct = document.getElementById("nombreProduct").value,
+                detalle = document.getElementById("detalle").value,
+                cantidad = document.getElementById("cantidad").value;
+                precio = document.getElementById("precio").value;
+                resultado = document.getElementById("resultado").value;
+            if(!checkEmptyInput()){
+                table.rows[rIndex].cells[0].innerHTML = nombreProduct;
+                table.rows[rIndex].cells[1].innerHTML = detalle;
+                table.rows[rIndex].cells[2].innerHTML = cantidad;
+                table.rows[rIndex].cells[3].innerHTML = precio;
+                table.rows[rIndex].cells[4].innerHTML = resultado;
+            }
+        }
+        
+        function removeSelectedRow()
+        {
+            table.deleteRow(rIndex);
+            // clear input text
+            document.getElementById("nombreProduct").value = "";
+            document.getElementById("detalle").value = "";
+            document.getElementById("cantidad").value = "";
+            document.getElementById("precio").value = "";
+            document.getElementById("resultado").value = "";
+        }
