c2cd6a2a53aa944a9a2001d71aa78b167020a57e
RobertCR <rob3rtcr@gmail.com>
2022-02-16 14:56:00 -0300

    modificacion iva

--- main.js
+++ main.js
@@ -10,18 +10,3 @@ $(function(){
     })
 });
 //------------------
-$(function() {
-    //Operaciones matemáticas
-    $('#calcular').click(function(e) {
-        e.preventDefault();
-      //Almaceno los valores de los inputs
-        var precio = $('#precio').val();
-      //Condiciona para que acepte solo números usando las expresiones regulares
-        if(precio.match(/^[0-9]+$/)){
-            //porcentaje
-              var resultado = parseFloat(precio) * 21 / 100;
-            }
-      //Muestro el resultado
-    $('#resultado').text(resultado);
-    });
-})
